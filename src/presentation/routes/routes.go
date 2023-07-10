74acf2ae8089b76debdf62c6290bec2022732789
andreas <andreas.sf93@gmail.com>
2023-07-09 21:10:18 -0300

    feat(presentation): Add update route

--- src/presentation/routes/routes.go
+++ src/presentation/routes/routes.go
@@ -7,11 +7,12 @@ import (
 )
 
 func Routes() {
-	// alterar rota / para o index
+
 	http.HandleFunc("/", controller.GetProducts)
 	http.HandleFunc("/add", controller.Add)
 	http.HandleFunc("/insert", controller.CreateProduct)
 	http.HandleFunc("/delete", controller.DeleteProduct)
 	http.HandleFunc("/edit", controller.Edit)
+	http.HandleFunc("/update", controller.UpdateProduct)
 
 }
